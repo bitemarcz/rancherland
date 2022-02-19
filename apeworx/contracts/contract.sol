37ed74db77ed06a399feedb959b1321661ed3b74
Mario Zamora <mario.a.zamora@outlook.com>
2022-02-18 18:21:46 -0700

    Starting off by writing the base of the contract

---
+++ apeworx/contracts/contract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.12;
+
+contract CattleBranding {
+    int registrationID; // this could also be the brand number automatically generated 
+    int registrationImage; // this needs to be a hashed value 
+    int areaCode;
+    string email; 
+    string brandOwnerFname;
+    string brandOwnerLname;
+    string streetAddress;
+    string city;
+    string state;
+
+
+
+}
