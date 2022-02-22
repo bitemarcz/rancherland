90a4ab7e1ba2ef6909e91b0b2ea537d2f902f5d6
Mario Zamora <mario.a.zamora@outlook.com>
2022-02-22 08:09:39 -0700

    Pushing up new changes

--- apeworx/contracts/contract.sol
+++ apeworx/contracts/contract.sol
@@ -5,16 +5,16 @@ pragma solidity ^0.8.12;
 contract CattleBranding {
 
 
-    stuct Branding { // Branding struct that will track user information and branding information
+    struct Brand { // Branding struct that will track user information and branding information
         int registrationID; // this could also be the brand number automatically generated 
-    bytes32 registrationImage; // this needs to be a hashed value 
-    int areaCode;
-    string email; 
-    string brandOwnerFname;
-    string brandOwnerLname;
-    string streetAddress;
-    string city;
-    string state;
+        bytes32 registrationImage; // this needs to be a hashed value 
+        int areaCode;
+        string email; 
+        string brandOwnerFname;
+        string brandOwnerLname;
+        string streetAddress;
+        string city;
+        string statel;
     }
     
 }
