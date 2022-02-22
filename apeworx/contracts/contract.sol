dadb17e38c873a974fb5c1394fcbf0d359cc5361
Mario Zamora <mario.a.zamora@outlook.com>
2022-02-22 08:10:22 -0700

    Adding small fixes

--- apeworx/contracts/contract.sol
+++ apeworx/contracts/contract.sol
@@ -6,15 +6,15 @@ contract CattleBranding {
 
 
     struct Brand { // Branding struct that will track user information and branding information
-        int registrationID; // this could also be the brand number automatically generated 
+        uint registrationID; // this could also be the brand number automatically generated 
         bytes32 registrationImage; // this needs to be a hashed value 
-        int areaCode;
+        uint areaCode;
         string email; 
         string brandOwnerFname;
         string brandOwnerLname;
         string streetAddress;
         string city;
-        string statel;
+        string state;
     }
     
 }
