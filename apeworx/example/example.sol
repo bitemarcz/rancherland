2b6b6ef8256a4e62e06ef4f5232874cc8d09a468
Mario Zamora <mario.a.zamora@outlook.com>
2022-03-01 13:27:37 -0700

    Ignoring .build folder in apeworx folder and pushing example folder which contains some example code

---
+++ apeworx/example/example.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.12;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16; // state variables stored on the blockchain also public
+    uint dnaModulus = 10 ** dnaDigits; // some state variables public
+
+    struct Zombie { // complex data structure 
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies; // array of zombies might need to use array to hold a lot same data
+
+    mapping (uint => address) public zombieToOwner; // mapping between owner and address
+    mapping (address => uint) ownerZombieCount; // mapping of address owner to number of x
+
+    function _createZombie(string memory _name, uint _dna) private { 
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1; 
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+    // Standard functions for creating zombies 
+
+}
+
+contract ZombieFeeding is ZombieFactory { // example of inheritane
+    
+}
