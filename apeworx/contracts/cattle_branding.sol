dc4281fb3c1b5dea38c568868627e0ad1b5726b5
Mario Zamora <mario.a.zamora@outlook.com>
2022-03-01 11:54:27 -0700

    Moving some state variables around

--- apeworx/contracts/cattle_branding.sol
+++ apeworx/contracts/cattle_branding.sol
@@ -4,29 +4,42 @@ pragma solidity ^0.8.12;
 
 contract CattleBrandingFactory {
 
+    event NewCattleBrand(uint cattleBrandId, bytes32 registrationImage, string brandDetails, string county, string ownerDetails);
 
-    struct CattleBrand { // Branding struct that will track user information and branding information
-        uint registrationID; // this could also be the brand number automatically generated
-        bytes32 registrationImage; // not sure which data type this needs to be based on the cattle brands 
-        uint areaCode;
-        string email; 
-        string brandOwnerFname;
-        string brandOwnerLname;
-        string streetAddress;
-        string city;
-        string state;
+    uint brandId; // public state variables these details are also used for selling brands can be tied later to NFT
+    uint randImageId;
+    uint Pnumber;
+    uint areaCode;
+    string brandOwnerFname;
+    string brandOwnerLname;
+    string streetAddress;
+    string city;
+    string state;
+    string email;
+
+
+    struct CattleBrand { // Branding struct that will track required branding information, received from search field
+        bytes32 registrationImage; // not sure which data type this needs to be based on the cattle brands maybe a string
+        string brandDetails;
+        string county;
+        string ownerDetails;
+        
     }
 
     CattleBrand[] public brandings; // this is going to be an array of cattle brands 
 
-    // will need to insert some type of mapping data structure.. possibly? 
+    mapping (uint => address) public cattleBrandToOwner;
+    mapping (address => uint) ownerCattleBrandCount;
 
-    function _createCattleBrand() private {
-
+    function _createCattleBrandId(bytes32 _registrationImage, string memory _brandDetails, string memory _county, string memory _ownerDetails) private {
+        brandId = brandings.push(CattleBrand(_registrationImage, _brandDetails, _county, _ownerDetails)); // somehow need to perform a conversion from tuple to brandId
+        cattleBrandToOwner[brandId] = msg.sender;
+        ownerCattleBrandCount[msg.sender]++;
+        emit NewCattleBrand(brandId, _registrationImage, _brandDetails, _county, _ownerDetails);
     }
 
     function _generageImageHash(bytes32 _registrationImage) private pure returns (uint) {
-        uint randImageId = uint(keccak256(abi.encodePacked(_registrationImage)));
+        randImageId = uint(keccak256(abi.encodePacked(_registrationImage)));
         return randImageId;
     }
 
