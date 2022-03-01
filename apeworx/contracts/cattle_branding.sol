627e471ff1f2addc1f18d385a78728094e7011cd
Mario Zamora <mario.a.zamora@outlook.com>
2022-03-01 13:20:53 -0700

    No errors in code, attempting to compile

--- apeworx/contracts/cattle_branding.sol
+++ apeworx/contracts/cattle_branding.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.12;
 
 contract CattleBrandingFactory {
 
-    event NewCattleBrand(uint cattleBrandId, bytes32 registrationImage, string brandDetails, string county, string ownerDetails);
+    event NewCattleBrand(uint cattleBrandId, uint registrationImage, string brandDetails, string county, string ownerDetails);
 
-    uint brandId; // public state variables these details are also used for selling brands can be tied later to NFT for selling brands
     uint randImageId;
     uint Pnumber;
     uint areaCode;
@@ -19,26 +18,28 @@ contract CattleBrandingFactory {
 
 
     struct CattleBrand { // Branding struct that will track required branding information, received from search field
-        bytes32 registrationImage; // not sure which data type this needs to be based on the cattle brands maybe a string
+        uint registrationImage; // not sure which data type this needs to be based on the cattle brands maybe a string
         string brandDetails;
         string county;
         string ownerDetails;
         
     }
+    
 
     CattleBrand[] public brandings; // this is going to be an array of cattle brands 
 
     mapping (uint => address) public cattleBrandToOwner;
     mapping (address => uint) ownerCattleBrandCount;
 
-    function _createCattleBrandId(bytes32 _registrationImage, string memory _brandDetails, string memory _county, string memory _ownerDetails) private {
-        brandId = brandings.push(CattleBrand(_registrationImage, _brandDetails, _county, _ownerDetails)); // somehow need to perform a conversion from tuple to unint
+    function _createCattleBrandId(uint _registrationImage, string memory _brandDetails, string memory _county, string memory _ownerDetails) private {
+        brandings.push(CattleBrand(_registrationImage, _brandDetails, _county, _ownerDetails)); // somehow need to perform a conversion from tuple to unint
+        uint brandId = brandings.length - 1;
         cattleBrandToOwner[brandId] = msg.sender;
         ownerCattleBrandCount[msg.sender]++;
         emit NewCattleBrand(brandId, _registrationImage, _brandDetails, _county, _ownerDetails);
     }
 
-    function _generageImageHash(bytes32 _registrationImage) private pure returns (uint) {
+    function _generageImageHash(uint _registrationImage) private returns (uint) {
         randImageId = uint(keccak256(abi.encodePacked(_registrationImage)));
         return randImageId;
     }
