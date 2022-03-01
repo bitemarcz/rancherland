5c9591b3ce95399b5eb3cb0c5e7f3f954da979fe
Mario Zamora <mario.a.zamora@outlook.com>
2022-03-01 10:40:28 -0700

    Pushing up more contract skeleton code for the functions required to do work on our contract

--- apeworx/contracts/cattle_branding.sol
+++ apeworx/contracts/cattle_branding.sol
@@ -2,12 +2,12 @@
 
 pragma solidity ^0.8.12;
 
-contract CattleBranding {
+contract CattleBrandingFactory {
 
 
-    struct Brand { // Branding struct that will track user information and branding information
-        uint registrationID; // this could also be the brand number automatically generated 
-        bytes32 registrationImage; // this needs to be a hashed value 
+    struct CattleBrand { // Branding struct that will track user information and branding information
+        uint registrationID; // this could also be the brand number automatically generated
+        bytes32 registrationImage; // not sure which data type this needs to be based on the cattle brands 
         uint areaCode;
         string email; 
         string brandOwnerFname;
@@ -15,6 +15,27 @@ contract CattleBranding {
         string streetAddress;
         string city;
         string state;
+    }
+
+    CattleBrand[] public brandings; // this is going to be an array of cattle brands 
+
+    // will need to insert some type of mapping data structure.. possibly? 
+
+    function _createCattleBrand() private {
+
+    }
+
+    function _generageImageHash(bytes32 _registrationImage) private pure returns (uint) {
+        uint randImageId = uint(keccak256(abi.encodePacked(_registrationImage)));
+        return randImageId;
+    }
+
+    function _verifyCattleBrand() private {
+
+    }
+
+    function _importCattleBrand() private {
+
     }
     
 }
